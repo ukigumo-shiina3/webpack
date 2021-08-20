75a88aaae5f69758018e6fd8987d50f333edc8a9
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-20 11:03:29 +0900

    ⚡webpack-dev-serverの設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     // distディレクトリ: バンドルした後のディレクトリで使われる
     filename: "bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
   },
 };
